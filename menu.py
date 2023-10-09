b1065ab9ae38b0191a0a2d2c72a645ee49e7d326
Ivan Zarate <ivan23.zarate25@gmail.com>
2023-10-09 00:18:59 -0300

    mejoras

--- menu.py
+++ menu.py
@@ -1,6 +1,44 @@
-import sys
-from mi_primer_paquete.clientes import Cliente
+from mi_primer_paquete.clientes import *
 
-cliente1=Cliente("Juan", 23, "Pass1")
+data=[]
 
-print(cliente1)
+def mostrar_menu():
+    print("1. Registro")
+    print("2. Ver información de contacto")
+    print("3. Salir")
+
+def validar_opcion(opcion):
+    return opcion in ['1', '2', '3']
+
+def registro():
+            
+    Cliente.usuario=input("Ingrese su nombre: ")
+    Cliente.edad=input("Ingrese su edad: ")
+    Cliente.contrasena=input("Ingrese su contraseña: ")
+
+    data.append({
+        'usuario':Cliente.usuario,
+        'edad':Cliente.edad,
+        'contrasena':Cliente.contrasena,
+    })
+    Cliente.dataBase(data)
+    print(Cliente)
+
+while True:
+    mostrar_menu()
+    opcion = input("Seleccione una opción: ")
+
+    if validar_opcion(opcion):
+        if opcion == '1':
+                registro()
+        elif opcion == '2':
+                Cliente.ver_informacion()
+        else:
+                print("¡Hasta luego!")
+                break
+    else:
+        print("Opción inválida. Intente nuevamente.")
+   
+   
+
+#print(Cliente.ver_informacion())
--- mi_primer_paquete/clientes.py
+++ mi_primer_paquete/clientes.py
@@ -1,11 +1,62 @@
+import json
+
 class Cliente:
     
    def __init__(self, usuario, edad, contrasena):
-     self.usuario= usuario
-     self.edad= edad
-     self.contrasena= contrasena
-
+      self.usuario= usuario
+      self.edad= int(edad)
+      self.contrasena= contrasena
+    
    def __str__(self) -> str:
-          return f"Nombre: {self.usuario}"
+          return f"Gracias por registrarse: {self.usuario}"
+           
+   def dataBase(data):
+        with open("clientes.json", "w") as file:
+          json.dump(data, file, indent=4)
+          file.write('\n')
+     
+   
+        
+   def ver_informacion():
+     with open("clientes.json", "r") as file:
+        contactos = file.readlines()
+        for contacto in contactos:
+            contacto_dict = json.loads(contacto)
+            formatContact=contacto_dict['cliente'][0]
+            print("Información del contacto:")
+            print(f"Nombre: {formatContact['usuario']}")
+            print(f"Edad: {formatContact['edad']}")
+            print(f"Contraseña: {formatContact['contrasena']}")
+            return 
+        
+   """def registro(usuario, contrasena):
+      try:
+          clientes[usuario]=contrasena
+          return usuario
+      except:
+        print('An exception occurred')"""
+        
+        
+   """def login(usuario, contrasena):
+      try:
+          for k in clientes.keys():
+              if k == usuario:
+                  if clientes[usuario]==contrasena:
+                      return f"Bienvenido {usuario} !"
+                  else:
+                      return "La contraseña ingresada no es correcta"
+      except:
+              print('An exception occurred')
+              
+              
+   def update(usuario):
+      try:
+          newPass=input("Ingrese su nueva contraseña: ")
+          for k in clientes.keys():
+              if k == usuario:
+                  clientes[usuario]=newPass
+                  return "Se ha guardado su nueva contraseña"
+      except:
+        print('An exception occurred')"""
 
 
